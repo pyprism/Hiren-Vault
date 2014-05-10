d545bc9f290a243c8060aea5a6f1e80715a9fe2d
pyprism <git.pyprism@gmail.com>
2014-05-10 17:44:50 +0600

    Routes now working

---
+++ routes/data.js
@@ -0,0 +1,3 @@
+/**
+ * Created by prism on 5/10/14.
+ */
--- routes/new.js
+++ routes/new.js
@@ -1,3 +1,21 @@
 /**
  * Created by prism on 5/8/14.
  */
+var express = require('express');
+
+var router = express.Router();
+var database = require('./database.js');
+
+
+router.get('/', function(req, res){
+    res.render('newdata', {
+        title: 'Add new data'
+    })
+});
+
+router.post('/', function(req, res){
+    database.create(req);
+    res.end();
+});
+
+module.exports = router;
