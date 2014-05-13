c6e2292914d35b782ec5c401bd1dc18c5774b7e6
pyprism <git.pyprism@gmail.com>
2014-05-13 19:01:16 +0600

    finally duplicate value detection worked

--- routes/database.js
+++ routes/database.js
@@ -1,3 +1,33 @@
 /**
  * Created by prism on 5/9/14.
  */
+
+var mongoose = require( 'mongoose' );
+var auth = require('../model/hiren-conf');
+
+exports.create = function(req){
+    var instance = new auth();
+    if(req.body.tag && req.body.email){
+        auth.findOne({ 'tag' : req.body.tag , 'email' : req.body.email}, function(err , duplicate){
+            if (!err){
+                if(!duplicate){
+                    instance.tag = req.body.tag;
+                    instance.email = req.body.email;
+                    instance.username= req.body.username;
+                    instance.createdOn = Date.now();
+                    instance.url = req.body.url;
+                    instance.password = req.body.password;
+                    instance.save(function(err){
+                        if(!err) console.log('Saved');
+
+                    });
+                    return "Save";
+
+            }       else return "Duplicate";
+        } else console.log(err);
+        });
+
+        }
+
+    };
+
