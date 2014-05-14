5b27957e498ff3164034d87a65f57be6960a2853
pyprism <git.pyprism@gmail.com>
2014-05-14 21:54:38 +0600

    Added post render

--- routes/new.js
+++ routes/new.js
@@ -5,7 +5,9 @@ var express = require('express');
 
 var router = express.Router();
 var database = require('./database.js');
+var EventEmitter = require("events").EventEmitter;
 
+var ee = new EventEmitter();
 
 router.get('/', function(req, res){
     res.render('newdata', {
@@ -14,8 +16,33 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-    database.create(req);
-    res.end();
+    var statusData;
+    database.create(req, ee);
+    ee.on('status', function(data){
+        statusData = data;
+        res.render('newdata', {
+            title : 'Add More Data',
+            status : statusData
+        })
+    });
+    /*res.render('newdata', {
+        title : 'Add More Data',
+        status : statusData
+    })*/
+    /*if (result == 'Save'){
+        res.redirect('newdata',{
+            title: 'Add More Data' ,
+            status: 'Your new data is saved.You can add some more.'
+        })
+    }
+    else if(result == 'Duplicate'){
+        res.redirect('newdata',{
+            title: 'Add More Data' ,
+            status: 'Your inserted a duplicate data.'
+        })
+    }*/
+    //console.log(result);
+    //res.end();
 });
 
 module.exports = router;
