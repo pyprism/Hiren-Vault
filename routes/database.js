840832af10f10bbee015a5be7416fdb177483460
pyprism <git.pyprism@gmail.com>
2014-05-16 19:47:17 +0600

    new database call added

--- routes/database.js
+++ routes/database.js
@@ -3,13 +3,13 @@
  */
 
 var mongoose = require( 'mongoose' );
-var auths = require('../model/hiren-conf');
-
+var database = require('../model/hiren-conf');
+var us = require('underscore')._;
 
 exports.create = function(req, ee){
-    var instance = new auths.auth();
+    var instance = new database.auth();
     if(req.body.tag && req.body.email){
-        auths.auth.findOne({ 'tag' : req.body.tag , 'email' : req.body.email}, function(err , duplicate){
+        database.auth.findOne({ 'tag' : req.body.tag , 'email' : req.body.email}, function(err , duplicate){
             if (!err){
                 if(!duplicate){
                     instance.tag = req.body.tag;
@@ -22,11 +22,11 @@ exports.create = function(req, ee){
                         if(!err) console.log('Saved');
 
                     });
-                    ee.emit('status', 'Your data is saved.Add some more?');
+                    ee.emit('status', 'Your data is saved. Add some more?');
 
             }       else {
                     console.log("Duplicate");
-                    ee.emit('status', 'You entered duplicate data.Add some more?');
+                    ee.emit('status', 'You entered duplicate data. Add some more?');
                 };
         } else console.log(err);
         });
@@ -35,3 +35,14 @@ exports.create = function(req, ee){
 
     };
 
+exports.group = function(ee){
+    var a = [];
+    database.auth.find({},{'tag' : 1 , '_id' : 0 },function(err , data){
+        for (var x = 0; x < data.length ; x++){
+            //a.push[data[x]];
+            a.push(data[x].tag)
+        }
+        ee.emit('group',us.uniq(a,false));
+
+    })
+}
