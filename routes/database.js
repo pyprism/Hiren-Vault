b2fe460e6169250a4ef8530e46baf45b43b261e2
pyprism <git.pyprism@gmail.com>
2014-05-18 17:11:17 +0600

    still not working , sometimes I hate asynchronous programming

--- routes/database.js
+++ routes/database.js
@@ -48,31 +48,64 @@ exports.group = function(ee){
 }
 
 exports.details = function(ee){
-
+    console.log("");
 }
 
-function x(){
-    var a = [],
-        b = [];
+/*function x(){
+    var a = [] ;
+       // b = [],
+       // tagname ;
     database.auth.distinct('tag',function(err , tagname){
-        //for (var x = 0; x < tagname.length ; x++){
-            a.push(tagname)
-            //a.push(data[x].tag)
-        //}
-       // console.log(a);
-       // ee.emit('group',us.uniq(a,false));
+        console.log(tagname)
+        //for (var y = 0; y < tagname.length ; y++)
+            a.push(tagname);
+        console.log("a =" ,a);
+    });
 
-    })
-    for(var x = 0; x< a.length ; x++){
+    *//*for(var x = 0; x< a.length ; x++){
         database.auth.count({ 'tag' : a[x] },function(err, nisha){
             // a[tagname[x]] = nisha.length ;
             //console.log(tagname[x] );
-            b[a[x]] = nisha;
+           // b[a[x]] = nisha;
+            console.log(nisha);
+
+        });*//*
+
+   // }*/
+//}
+/*function z(){
+    var a = [];
+    database.auth.distinct('tag',function(err , tagname){
+        console.log(tagname); // printing tag names perfectly
+        a.push(tagname);
+        console.log("a =" ,a); //
+        for(var x = 0; x< a.length ; x++){  //so this block is not working .
+            database.auth.count({ 'tag' : a[x] },function(err, nish){
+                // a[tagname[x]] = nish.length ;
+            });
 
         });
-        console.log(b);
-    }
+        console.log("a =" ,a); // printing an empty array
+    });
+}*/
 
+function y(){
+    var a = [] ,
+        b = [];
+    database.auth.distinct('tag', function(err, tagname){
+        for(var y =0; y< tagname.length; y++){
+            a.push(tagname[y]);
+        }
+
+        console.log("a =" , a.length);
+        for(var x = 0;x < a.length; x++){
+            database.auth.count({ 'tag' : a[x]} , function(err, nish){
+               // b[tagname[x]] = nish.length;
+                console.log(a[x]);
+            });
+        }
+        //console.log(b)
+    });
 }
 
-x()
+y()
