f745d52ee47ec9c847d16e98e901a9c332338aa3
pyprism <git.pyprism@gmail.com>
2014-08-23 21:55:06 +0600

    some fix

--- hiren/password/models.py
+++ hiren/password/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Password(models.Model):
+    tags = models.CharField(max_length=30)
+    site_url = models.URLField(max_length=200)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=300)
+    added_at = models.DateTimeField()
+    updated_at = models.DateTimeField(blank=True)
