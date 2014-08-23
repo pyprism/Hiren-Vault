9976961c4cd0a2e1d615e14f57fd717f3bd138d9
pyprism <git.pyprism@gmail.com>
2014-08-23 21:54:10 +0600

    not working :/

--- hiren/password/crypt.py
+++ hiren/password/crypt.py
@@ -1,1 +1,28 @@
-__author__ = 'prism'
+from Crypto.Cipher import AES
+import hashlib
+
+
+def pad(s):
+    return s + ((16-len(s) % 16) * "{")
+
+
+def digest(key):
+    m = hashlib.sha256()
+    m.update(str.encode(key))
+    return m.digest()
+
+
+def encrypt(plantext, key):
+    cipher = AES.new(digest(key))
+    return cipher.encrypt(pad(plantext))
+
+
+def decrypt(ciphertext, key):
+    cipher = AES.new(digest(key))
+    dec = cipher.decrypt(ciphertext).decode('utf-8')
+    l = dec.count('{')
+    return dec[:len(dec)-l]
+
+x = encrypt("hello", "@##4edff")
+print(x)
+print(decrypt(x, "nisha"))
