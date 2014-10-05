1630d0b6e0e13cc53cfd7b3a056147335853358d
pyprism <git.pyprism@gmail.com>
2014-10-05 21:22:04 +0600

    Updated models

--- hiren/password/models.py
+++ hiren/password/models.py
@@ -4,10 +4,14 @@ from django.db import models
 
 
 class Password(models.Model):
-    tags = models.CharField(max_length=30)
+    tags = models.CharField(max_length=30, blank=True, null=True)
     site_url = models.URLField(max_length=200)
-    username = models.CharField(max_length=100)
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=300)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    password = models.CharField(max_length=500)
+    note = models.TextField(blank=True, null=True)
     added_at = models.DateTimeField()
-    updated_at = models.DateTimeField(blank=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.site_url, self.password)
