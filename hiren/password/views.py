c32e6d08350606a69ca9cbd8262347c5710bae42
pyprism <git.pyprism@gmail.com>
2014-10-10 19:21:19 +0600

    added new view

--- hiren/password/views.py
+++ hiren/password/views.py
@@ -70,15 +70,69 @@ def show(request, ids):
         return render(request, 'show.html', {'id': ids})
 
 
-def decrypt(request, ids):
+def update(request, ids):
     if request.user.is_authenticated():
-        pass
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+            url = request.POST.get('url')
+            tag = request.POST.get('tag')
+            note = request.POST.get('note')
+            key = request.POST.get('key')
+            data = Password.objects.get(id=ids)
+            if password == data.password:   # wtf duplicate code !!
+                obj = Password.objects.get(id=ids)
+                obj.username = username
+                obj.email = email
+                obj.site_url = url
+                obj.tags = tag
+                obj.note = note
+                obj.updated_at = timezone.now()
+                obj.save()
+                messages.info(request, 'Entry updated')
+                return request('/browse')
+            else:
+                encrypt = Secret()
+                encrypted = encrypt.encrypt(password, key)
+                obj = Password.objects.get(id=ids)
+                obj.username = username
+                obj.email = email
+                obj.site_url = url
+                obj.tags = tag
+                obj.note = note
+                obj.password = encrypted
+                obj.updated_at = timezone.now()
+                obj.save()
+                messages.info(request, 'Entry updated')
+                return redirect("/browse")
 
 
 def edit(request, ids):
     if request.user.is_authenticated():
-        data = Password.objects.filter(id=ids)
-        return render(request, 'edit.html', {'data': data[0]})
+        if request.method == "POST":
+            key = request.POST.get('key')
+            data = Password.objects.get(id=ids)
+            decrypt = Secret()
+            if decrypt.decrypt(data.password, key):
+                password = decrypt.decrypt(data.password, key)
+                return render(request, 'edit.html', {'data': data, 'password': password})
+            else:
+                messages.error(request, 'Your key is not correct')
+                return render(request, 'edit.html', {'data': data})
+        else:
+            data = Password.objects.get(id=ids)
+            return render(request, 'edit.html', {'data': data})
+
+
+def delete(request):
+    if request.user.is_authenticated():
+        if request.method == "POST":
+            ids = request.POST.get('id')
+            obj = Password.objects.get(id=ids)
+            obj.delete()
+            messages.info(request, 'Entry deleted')
+            return redirect('/browse')
 
 
 def reveal(request):
@@ -86,11 +140,11 @@ def reveal(request):
         if request.method == "POST":
             ids = request.POST.get('id')
             key = request.POST.get('key')
-            data = Password.objects.filter(id=ids)
+            data = Password.objects.get(id=ids)
             decrypt = Secret()
-            if decrypt.decrypt(data[0].password, key):
-                password = decrypt.decrypt(data[0].password, key)
-                return render(request, 'result.html', {'data': data[0], 'password': password})
+            if decrypt.decrypt(data.password, key):
+                password = decrypt.decrypt(data.password, key)
+                return render(request, 'result.html', {'data': data, 'password': password})
             else:
                 messages.error(request, 'Your key is not correct')
                 return redirect('/id/show' + ids)
