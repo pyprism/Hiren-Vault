e9e7bf705648602f0347d849d1de3e3c2f400b8b
prism <git.pyprism@gmail.com>
2015-01-23 22:48:37 +0600

    changed to OOP

--- hiren/password/functional_tests.py
+++ hiren/password/functional_tests.py
@@ -1,8 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-binary = FirefoxBinary('./firefox_33.0/firefox')
-browser = webdriver.Firefox(firefox_binary=binary)
-browser.get('http://localhost:8000')
 
-assert 'Hiren' in browser.title
+class Hiren(unittest.TestCase):
+    binary = FirefoxBinary('./firefox_33.0/firefox')
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(firefox_binary=self.binary)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_index(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Hiren', self.browser.title)
+        print(self.browser.title)
+        self.fail('Finish title test')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
