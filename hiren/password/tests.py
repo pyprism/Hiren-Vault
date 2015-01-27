d4a386b2ae0fda645f12a11d594e4b107a999d1d
prism <git.pyprism@gmail.com>
2015-01-27 18:26:31 +0600

    simple test

--- hiren/password/tests.py
+++ hiren/password/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import resolve
+from .views import index
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolve_to_index_view(selfs):
+        found = resolve('/')
+        selfs.assertEqual(found.func, index)
