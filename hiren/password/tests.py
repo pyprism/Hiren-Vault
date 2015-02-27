01c830495d36507fdd8029248407c56b38da1e76
Ashutosh Das <git.pyprism@gmail.com>
2015-02-28 00:15:28 +0600

    :|

--- hiren/password/tests.py
+++ hiren/password/tests.py
@@ -1,11 +1,17 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from .views import index
 # Create your tests here.
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_index_view(selfs):
+    def test_root_url_resolve_to_index_view(self):
         found = resolve('/')
-        selfs.assertEqual(found.func, index)
+        self.assertEqual(found.func, index)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
