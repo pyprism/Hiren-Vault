6518560e9943edaacc8bfd0d6aa07be02bc577d2
Ashutosh Das <git.pyprism@gmail.com>
2015-05-11 21:16:23 +0600

    added models

--- password/models.py
+++ password/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Password(models.Model):
+    tags = models.CharField(max_length=30, blank=True, null=True)
+    site_url = models.URLField(max_length=200)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    password = models.CharField(max_length=500)
+    note = models.TextField(blank=True, null=True)
+    added_at = models.DateTimeField()
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.site_url, self.password)
+
+
+class Recent(models.Model):
+    obj = models.IntegerField(blank=True, null=True)
