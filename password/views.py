28e99aad52eef8e99be8748e0691e46c56c2652e
Ashutosh Das <git.pyprism@gmail.com>
2015-05-13 09:15:26 +0600

    1st class based view :smile:

--- password/views.py
+++ password/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
+'''
+Date : 12 May, 2015
+'''
 # Create your views here.
+
+
+class Authentication(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("sas")
