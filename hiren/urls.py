1f5cabd77dc9b91ab4287012929a9e941fe2c794
Ashutosh Das <git.pyprism@gmail.com>
2015-05-13 09:14:53 +0600

    added route

--- hiren/urls.py
+++ hiren/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from password import urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include(urls))
 ]
