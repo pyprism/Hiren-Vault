2bd5e274d011d5338c75a563afb62b9da3ab7dea
Ashutosh Das <git.pyprism@gmail.com>
2015-05-26 17:25:15 +0600

    added new url

--- password/urls.py
+++ password/urls.py
@@ -4,9 +4,10 @@ Date : 12 May, 2015
 '''
 from django.conf.urls import include, url
 #from .views import Authentication
-from .views import Login, Logout
+from .views import Login, Logout, AddData
 
 urlpatterns = [
     url(r'^$', Login.as_view(), name='auth'),
-    url(r'^logout/$', Logout.as_view())
+    url(r'^logout/$', Logout.as_view()),
+    url(r'^add/$', AddData.as_view())
 ]
