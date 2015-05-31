f9d4c3d1ed66c3214fc645b458772cab12eebe8e
Ashutosh Das <git.pyprism@gmail.com>
2015-05-31 19:41:20 +0600

    final urls

--- password/urls.py
+++ password/urls.py
@@ -3,11 +3,18 @@ __author__ = 'prism'
 Date : 12 May, 2015
 '''
 from django.conf.urls import include, url
-#from .views import Authentication
-from .views import Login, Logout, AddData
+from .views import Login, Logout
 
 urlpatterns = [
     url(r'^$', Login.as_view(), name='auth'),
     url(r'^logout/$', Logout.as_view()),
-    url(r'^add/$', AddData.as_view())
+    url(r'^add', 'password.views.add'),
+    url(r'^browse$', 'password.views.browse'),
+    url(r'^id/(?P<ids>\d+)/show$', 'password.views.show'),
+    url(r'^id/(?P<ids>\d+)/update$', 'password.views.update'),
+    url(r'^id/(?P<ids>\d+)/edit$', 'password.views.edit'),
+    url(r'^id/(?P<ids>\d+)/edit$', 'password.views.edit'),
+    url(r'^delete$', 'password.views.delete'),
+    url(r'^reveal$', 'password.views.reveal'),
+    url(r'^search$', 'password.views.search'),
 ]
