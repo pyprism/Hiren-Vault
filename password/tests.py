94d2d06892eff1cb4061d3bfee35a4afe3bc4af2
Ashutosh Das <git.pyprism@gmail.com>
2015-06-04 17:08:10 +0600

    still learning

--- password/tests.py
+++ password/tests.py
@@ -3,8 +3,12 @@ import unittest
 from selenium import webdriver
 from django.core.urlresolvers import resolve
 from .views import login, browse
+from django.template.loader import render_to_string
 from django.http import HttpRequest
+from django.contrib.auth.models import User
 import os
+import time
+from selenium.webdriver.common.keys import Keys
 # Create your tests here.
 
 
@@ -23,6 +27,7 @@ class LoginFunctionalTest(unittest.TestCase):
         else:
             self.browser = webdriver.Firefox()
             self.browser.implicitly_wait(3)
+            # self.browser.get('http://localhost:8000')
 
     def tearDown(self):
         self.browser.quit()
@@ -31,7 +36,20 @@ class LoginFunctionalTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('Hiren->Login', self.browser.title)
-       # self.fail('Finish the test')
+
+    def test_login_form(self):
+        self.browser.get('http://localhost:8000')
+        user = User.objects.create_superuser('testHiren', 'myemail@test.com', 'testPass')
+        user.save()
+        username = self.browser.find_element_by_id('username-id')
+        password = self.browser.find_element_by_id('password-id')
+        submit = self.browser.find_element_by_id('login-button')
+        username.send_keys('testHiren')
+        password.send_keys('testPass')
+        submit.click()
+        time.sleep(10)
+        location = self.browser.current_url
+        self.assertEqual('http://localhost:8000/dashboard', location)
 
 #class LoginPageTest(TestCase):
 
@@ -52,4 +70,10 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
         self.assertIn(b'<title>Hiren->Login</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_home_page_returns_correct_template(self):
+        request = HttpRequest()
+        response = login(request)
+        expected_html = render_to_string('index.html')
+        self.assertEqual(response.content.decode(), expected_html)
 
