9d242e1b40a01fb71cc243856c907c25e9f9dc99
Ashutosh Das <git.pyprism@gmail.com>
2015-06-04 21:32:41 +0600

    added models test

--- password/tests.py
+++ password/tests.py
@@ -6,9 +6,12 @@ from .views import login, browse
 from django.template.loader import render_to_string
 from django.http import HttpRequest
 from django.contrib.auth.models import User
+from .models import Password, Tag
 import os
 import time
+from password import crypto
 from selenium.webdriver.common.keys import Keys
+from django.utils import timezone
 # Create your tests here.
 
 
@@ -77,3 +80,27 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('index.html')
         self.assertEqual(response.content.decode(), expected_html)
 
+
+class ModelTest(TestCase):
+
+    def setUp(self):
+        passwd = Password.objects.create(site_url='https://hiren.lol', username='hiren', email='hiren@hiren.com',
+                                         password='secretPass', note='my_note', added_at=timezone.now())
+        Tag.objects.create(password=passwd, tags='test Tag')
+
+    def test_password_model(self):
+        passwd_items = Password.objects.all()
+        self.assertEqual(passwd_items.count(), 1)
+
+        pass_result =Password.objects.get(email='hiren@hiren.com')
+        self.assertEqual(pass_result.email, 'hiren@hiren.com')
+
+    def test_tag_model(self):
+        tags_item = Tag.objects.all()
+        self.assertEqual(tags_item.count(), 1)
+
+        tags_result = Tag.objects.get(tags='test Tag')
+        self.assertEqual(tags_result.tags, 'test Tag')
+
+        pass_id = Password.objects.get(email='hiren@hiren.com')
+        self.assertEqual(tags_result.password, pass_id)
