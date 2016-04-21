e1704d13f74b86f4eec793b7cd2ef83be97e697e
Ashutosh Das <git.pyprism@gmail.com>
2016-04-21 21:44:09 +0600

    Added new views

--- password/views.py
+++ password/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .models import Vault, Recent, Tag
+from .serializers import VaultSerializer, TagSerializer, RecentSerializer
 
-# Create your views here.
+
+class VaultViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Vault.objects.all()
+    serializer_class = VaultSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
