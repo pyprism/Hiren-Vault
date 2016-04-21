2975b0532cca135d6d0a4eaa850f9807403ba320
Ashutosh Das <git.pyprism@gmail.com>
2016-04-21 21:43:30 +0600

    Added new route

--- hiren/urls.py
+++ hiren/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from django.views.generic import TemplateView
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework import routers
+from password import views
+
+router = routers.DefaultRouter()
+router.register('vault', views.VaultViewSet)
+router.register('tag', views.TagViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
