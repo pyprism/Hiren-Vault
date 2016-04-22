86ea7ae53008ea4c943bc81c0ad7ea4098d2685b
Ashutosh Das <git.pyprism@gmail.com>
2016-04-22 11:50:06 +0600

    altered fields

--- password/models.py
+++ password/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 
 class Vault(models.Model):
-    site_url = models.URLField(max_length=200)
-    username = models.CharField(max_length=100, null=True)
-    email = models.CharField(max_length=100, null=True)
-    password = models.CharField(max_length=200)
+    site_url = models.URLField(max_length=600)
+    username = models.CharField(max_length=500, null=True)
+    email = models.CharField(max_length=500, null=True)
+    password = models.CharField(max_length=500)
     note = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tag = models.ForeignKey('Tag', null=True)
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
-    vault = models.ForeignKey('Vault', on_delete=models.CASCADE)
+    name = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Recent(models.Model):
