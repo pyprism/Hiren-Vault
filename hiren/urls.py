af1d095aec6731d74491c01ccf48a17d09b5675b
Ashutosh Das <git.pyprism@gmail.com>
2016-07-14 23:20:57 +0600

    added new route

--- hiren/urls.py
+++ hiren/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 from rest_framework import routers
 from password import views
 
@@ -23,10 +23,13 @@ router = routers.DefaultRouter()
 router.register('vault', views.VaultViewSet)
 router.register('tag', views.TagViewSet)
 router.register('recent', views.RecentViewSet)
+router.register('secret', views.SecretViewset)
 
 urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
