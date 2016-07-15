7a184f69d373d455d1e5dda3a6f87b770b9109d2
Ashutosh Das <git.pyprism@gmail.com>
2016-07-15 23:48:25 +0600

    Added new func and routes

--- static/apps/app.jsx
+++ static/apps/app.jsx
@@ -4,6 +4,8 @@ import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import Login from './components/Login.jsx';
 import Main from './components/Main.jsx';
 import Secret from './components/Secret.jsx';
+import Passwords from './components/Passwords.jsx';
+import PasswordForm from './components/PasswordForm.jsx';
 import axios from 'axios';
 
 
@@ -26,12 +28,21 @@ function authRequired(nextState, replace) {
 
 }
 
+function keyRequired(nextState, replace){
+    if(!sessionStorage.getItem('key')) {
+        replace('/dashboard/secret/');
+        sweetAlert("Oops!", 'Secret Key Required', "info");
+    }
+}
+
 
 ReactDOM.render(
     <Router history={browserHistory} >
         <Route path="/" component={Login} />
         <Route path="/key" onEnter={authRequired} component={Secret} />
-        <Route path="/dashboard" onEnter={authRequired} component={Main}>
+        <Route path="/app" onEnter={authRequired} component={Main}>
+            <Route path="passwords" onEnter={keyRequired} component={Passwords} />
+            <Route path="password/create" onEnter={keyRequired} component={PasswordForm} />
         </Route>
     </Router>,
     document.getElementById('app')
