ae70ec5c7807ee9eeff4e375f1fe8d1f7cb61269
Ashutosh Das <git.pyprism@gmail.com>
2016-07-24 23:10:29 +0600

    fixed auto complete

--- static/apps/components/PasswordForm.jsx
+++ static/apps/components/PasswordForm.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Helmet from "react-helmet";
 import axios from 'axios';
-import { browserHistory } from 'react-router';
+import {encrypt, decrypt} from '../utils/pgp.js';
+import {encryptAsync} from '../utils/async_pgp.js';
 
 
 export default class PasswordForm extends React.Component {
 
     editor(e){
-        (async function () {
-            e.preventDefault();
-        })();
+        console.log('s');
+        e.preventDefault();
+        let x = encryptAsync(sessionStorage.getItem('key'), ReactDOM.findDOMNode(this.refs.tag).value);
+        console.log(x);
+           /* axios({
+                method: 'post',
+                url: '/api/tag/',
+                data: {'name': ReactDOM.findDOMNode(this.refs.tag).value},
+                headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+            }).then(function(response) {
+                $.notify("Tag Created", "success");
+            }).catch(function (err) {
+                sweetAlert("Oops!", err.data.name[0], "error");
+            });*/
     }
 
     componentDidMount(){
@@ -27,9 +39,21 @@ export default class PasswordForm extends React.Component {
         })();
 
         (function () {  // function for tag autocomplete
-            var input = document.getElementsByClassName("tag");
-            new Awesomplete(input, {
-                list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+            var input = document.getElementById("tag");
+            var awesomplete = new Awesomplete(input);
+            axios({
+                method: 'get',
+                url: '/api/tag/',
+                headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+            }).then(function (response) {
+                let bunny = [];
+                response.data.forEach(function (data) {
+                    bunny.push(data.name);
+                });
+                awesomplete.list = bunny;
+            }).catch(function (response) {
+                sweetAlert("Oops!", response.data, "error");
+                console.error(response);
             });
         })();
     }
@@ -38,7 +62,7 @@ export default class PasswordForm extends React.Component {
         return (
             <div>
                 <Helmet
-                    title="Vault::Save Password"
+                    title="Vault->Save Password"
                 />
 
                 <form className="form col-md-12 center-block" onSubmit={this.editor.bind(this)} >
@@ -56,7 +80,7 @@ export default class PasswordForm extends React.Component {
                     </div>
                     <div className="form-group">
                         <label >Tag</label>
-                        <input type="text"  ref="tag" className="form-control awesomplete tag" />
+                        <input type="text"  ref="tag" id="tag" className="form-control awesomplete tag" />
                     </div>
                     <div className="form-group">
                         <label >Password</label>
@@ -68,14 +92,6 @@ export default class PasswordForm extends React.Component {
                     <div className="form-group">
                         <label >Optional Note</label>
                         <textarea  ref="note" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <div className='input-group date' id='datetime' >
-                            <div className="input-group-addon">
-                                <span className="glyphicon glyphicon-calendar" />
-                            </div>
-                            <input type='text' className="form-control" ref="date" required  placeholder="Date"/>
-                        </div>
                     </div>
                     <div className="form-group">
                         <button type="submit" className="btn btn-primary btn-lg btn-block">Save</button>
