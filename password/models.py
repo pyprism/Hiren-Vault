5e95032775f0099c611f1684b3e6474ace0904a0
Ashutosh Das <git.pyprism@gmail.com>
2016-07-24 23:09:37 +0600

    altered fields

--- password/models.py
+++ password/models.py
@@ -13,7 +13,7 @@ class Vault(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=500)
+    name = models.CharField(max_length=500, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
