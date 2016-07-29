5d95ce2cf9bc26b7ba6b9f3c7eb6ca2557af1f93
Ashutosh Das <git.pyprism@gmail.com>
2016-07-29 22:06:18 +0600

    :/

---
+++ static/apps/ajax/passwords.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import {encrypt, decrypt} from '../utils/pgp.js'
+import {observable, action, computed} from "mobx";
+
+export default class PasswordsAjax {
+	@observable bunny = {};
+
+	@action loadBunny(bunny) {
+    	this.bunny = bunny;
+    }
+
+	bunnies = async() => {
+	 let xoxo = await axios.get('/api/vault/', {
+	 	headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+    });
+	 this.loadBunny(xoxo);
+	}
+
+    @computed get logg() {
+    	console.log(this.bunny);
+    }
+}
+
+/** export async function passwords(){
+    let bunny = {};
+    bunny = await axios.get('/api/vault/', {
+    	headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+    });
+
+    axios({
+        method: 'get',
+        url: '/api/vault/',
+        headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+    }).then(function(response) {
+        response.data.forEach(function(data) {
+            let temp = {};
+            temp['id'] = data.id;
+            temp['site_url'] = data.site_url;
+            temp['email'] = await decrypt(sessionStorage.getItem('key'), data.email);
+            temp['username'] = await decrypt(sessionStorage.getItem('key'), data.username);
+            temp['password'] = await decrypt(sessionStorage.getItem('key'), data.password);
+            temp['note'] = await decrypt(sessionStorage.getItem('key'), data.note);
+            temp['tag'] = await decrypt(sessionStorage.getItem('key'), data.tag);
+            temp['created_at'] = data.created_at;
+            temp['updated_at'] = data.updated_at;
+            bunny.push(temp);
+        });
+    }).catch(function (err) {
+        sweetAlert("Oops!", err.data, "error");
+    }); 
+    console.log(bunny); 
+} **/
