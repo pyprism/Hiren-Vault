df597c58190d87a7a17513d1f4da62eea90979fd
Ashutosh Das <git.pyprism@gmail.com>
2016-07-30 21:29:09 +0600

    :/

--- static/apps/components/Passwords.jsx
+++ static/apps/components/Passwords.jsx
@@ -4,30 +4,15 @@ import Helmet from "react-helmet";
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-//import  PasswordsAjax  from '../ajax/passwords.js';
 import {encrypt, decrypt} from '../utils/pgp.js'
 
 
 
 export default class Passwords extends React.Component {
 
-	constructor(props) {
-		super(props);
-		let temp_bunny = {};
-
-		(function () {
-			axios({
-        method: 'get',
-        url: '/api/vault/',
-        headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
-    }).then(function(response) {
-    	this.temp_bunny.push(response.data);
-    }.bind(this)).catch(function (err) {
-    	console.error(err);
-        //sweetAlert("Oops!", err.data, "error");
-    }); 
-		})();
-	}
+	//constructor(props) {
+	//	super(props);
+	//}
 
 	componentDidMount() {
 		
@@ -36,12 +21,27 @@ export default class Passwords extends React.Component {
 	}
 
 	render() {
+
+		let bunny = [
+		{
+			'id' : 1,
+			'date' : 's'
+		},
+		{
+			'id' : 3,
+			'date' : 'ssas'
+		}
+		];
+
 		return (
 			<div>
-				<Helmet
-                    title="Vault-> Passwords"
-                />
-				I am passwords
+			<Helmet
+			title="Vault-> Passwords"
+			/>
+			<BootstrapTable data={bunny} striped={true} hover={true}>
+				<TableHeaderColumn isKey={true} dataField="id">ID</TableHeaderColumn>
+				<TableHeaderColumn dataField="date">date</TableHeaderColumn>
+			</BootstrapTable>
 			</div>
 			)
 	}
