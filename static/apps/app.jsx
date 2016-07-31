803f3e3f1edb03a4a4a487bbf06ab674f17da427
Ashutosh Das <git.pyprism@gmail.com>
2016-07-31 23:22:27 +0600

    added props

--- static/apps/app.jsx
+++ static/apps/app.jsx
@@ -9,6 +9,7 @@ import PasswordForm from './components/PasswordForm.jsx';
 import Tags from './components/Tags.jsx';
 import {notFound} from './components/404.jsx';
 import axios from 'axios';
+import {appState} from './store/passwords.js';
 
 
 function authRequired(nextState, replace) {
@@ -43,7 +44,7 @@ ReactDOM.render(
         <Route path="/" component={Login} />
         <Route path="/key" onEnter={authRequired} component={Secret} />
         <Route path="/app" onEnter={authRequired} component={Main}>
-            <Route path="passwords" onEnter={keyRequired} component={Passwords} />
+            <Route path="passwords" store={appState} onEnter={keyRequired} component={Passwords} />
             <Route path="password/create" onEnter={keyRequired} component={PasswordForm} />
             <Route path="tags" onEnter={keyRequired} component={Tags} />
             <Route path="*" component={notFound} />
