112ca38b986e9df60b4ed06609f7f4136c4e3435
Ashutosh Das <git.pyprism@gmail.com>
2016-07-31 23:23:13 +0600

    still exploring :/

--- static/apps/components/Passwords.jsx
+++ static/apps/components/Passwords.jsx
@@ -5,19 +5,21 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import {encrypt, decrypt} from '../utils/pgp.js'
+import {observer} from 'mobx-react';
 
 
-
+@observer
 export default class Passwords extends React.Component {
 
-	//constructor(props) {
-	//	super(props);
-	//}
+	constructor(props) {
+		super(props);
+		//this.props = this.props.bind(this);
+	}
 
-	componentDidMount() {
+	componentDidMount = () => {
 		
 
-		//console.log(bunny);
+		this.props.routes[1].store.fetch();
 	}
 
 	render() {
@@ -38,9 +40,9 @@ export default class Passwords extends React.Component {
 			<Helmet
 			title="Vault-> Passwords"
 			/>
-			<BootstrapTable data={bunny} striped={true} hover={true}>
+			<BootstrapTable data={this.props.routes[1].store.bunny} striped={true} hover={true}>
 				<TableHeaderColumn isKey={true} dataField="id">ID</TableHeaderColumn>
-				<TableHeaderColumn dataField="date">date</TableHeaderColumn>
+				<TableHeaderColumn dataField="updated_at">Date</TableHeaderColumn>
 			</BootstrapTable>
 			</div>
 			)
