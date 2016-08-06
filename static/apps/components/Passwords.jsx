c9940b676eadb93cfd91b03c71cef9debd986b47
Ashutosh Das <git.pyprism@gmail.com>
2016-08-06 23:50:40 +0600

    :/

--- static/apps/components/Passwords.jsx
+++ static/apps/components/Passwords.jsx
@@ -14,10 +14,39 @@ export default class Passwords extends React.Component {
 	constructor(props) {
 		super(props);
 		//this.props = this.props.bind(this);
+		this.x = 4;
 	}
 
-	componentDidMount = () => {
-		
+	componentWillMount = () => {
+
+		/*axios({
+			method: 'get',
+			url: '/api/vault/',
+			headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+		}).then(function (response) {
+			response.data.forEach(function(data) {
+				let temp = {};
+				temp['id'] = data.id;
+				temp['site_url'] = data.site_url;
+				temp['created_at'] = data.created_at;
+				temp['updated_at'] = data.updated_at;
+				[data.email, data.username, data.password, data.note].forEach(function(element, index, array) {
+					console.log(element + "");
+					//if(element + '' == 'data.email')
+					//	console.log('email');
+					//decrypt(sessionStorage.getItem('key'), element).then(function(decrypted) {
+					//	console.log(decrypted);
+				//	}).catch(function (error) {
+     			//		console.error(error);
+ 				//	});
+        			//console.log(z);
+        		});
+			});
+		})
+		.catch(function (response) {
+			console.error(response);
+			//sweetAlert("Oops!", response.data, "error");
+		});*/
 
 		this.props.routes[1].store.fetch();
 	}
@@ -41,8 +70,8 @@ export default class Passwords extends React.Component {
 			title="Vault-> Passwords"
 			/>
 			<BootstrapTable data={this.props.routes[1].store.bunny} striped={true} hover={true}>
-				<TableHeaderColumn isKey={true} dataField="id">ID</TableHeaderColumn>
-				<TableHeaderColumn dataField="updated_at">Date</TableHeaderColumn>
+			<TableHeaderColumn isKey={true} dataField="id">ID</TableHeaderColumn>
+			<TableHeaderColumn dataField="updated_at">Date</TableHeaderColumn>
 			</BootstrapTable>
 			</div>
 			)
