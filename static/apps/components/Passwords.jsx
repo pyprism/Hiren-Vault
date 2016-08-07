d43daded89e5751b9f4d6ae46a3925f986b2308c
Ashutosh Das <git.pyprism@gmail.com>
2016-08-07 23:50:12 +0600

    :/

--- static/apps/components/Passwords.jsx
+++ static/apps/components/Passwords.jsx
@@ -6,72 +6,42 @@ import { browserHistory } from 'react-router';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import {encrypt, decrypt} from '../utils/pgp.js'
 import {observer} from 'mobx-react';
+import {toJS} from 'mobx';
 
 
 @observer
 export default class Passwords extends React.Component {
 
-	constructor(props) {
-		super(props);
-		//this.props = this.props.bind(this);
-		this.x = 4;
-	}
-
-	componentWillMount = () => {
-
-		/*axios({
-			method: 'get',
-			url: '/api/vault/',
-			headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
-		}).then(function (response) {
-			response.data.forEach(function(data) {
-				let temp = {};
-				temp['id'] = data.id;
-				temp['site_url'] = data.site_url;
-				temp['created_at'] = data.created_at;
-				temp['updated_at'] = data.updated_at;
-				[data.email, data.username, data.password, data.note].forEach(function(element, index, array) {
-					console.log(element + "");
-					//if(element + '' == 'data.email')
-					//	console.log('email');
-					//decrypt(sessionStorage.getItem('key'), element).then(function(decrypted) {
-					//	console.log(decrypted);
-				//	}).catch(function (error) {
-     			//		console.error(error);
- 				//	});
-        			//console.log(z);
-        		});
-			});
-		})
-		.catch(function (response) {
-			console.error(response);
-			//sweetAlert("Oops!", response.data, "error");
-		});*/
-
+	componentDidMount = () => {
 		this.props.routes[1].store.fetch();
 	}
 
 	render() {
 
-		let bunny = [
-		{
-			'id' : 1,
-			'date' : 's'
-		},
-		{
-			'id' : 3,
-			'date' : 'ssas'
-		}
-		];
+		function anchor(cell, row){
+			let a;
+			if (cell.startsWith('http://'))
+				a = cell.slice(7);
+			else if (cell.startsWith('https://'))
+				a = cell.slice(8);
+			return '<a href=' + cell + '>' + a + '</a>' ;
+  		}
 
 		return (
 			<div>
 			<Helmet
 			title="Vault-> Passwords"
 			/>
-			<BootstrapTable data={this.props.routes[1].store.bunny} striped={true} hover={true}>
-			<TableHeaderColumn isKey={true} dataField="id">ID</TableHeaderColumn>
-			<TableHeaderColumn dataField="updated_at">Date</TableHeaderColumn>
+			<BootstrapTable data={toJS(this.props.routes[1].store.bunny)} striped={true} hover={true} condensed={true} pagination={true} search={true}>
+				<TableHeaderColumn dataField="id" isKey={true}>ID</TableHeaderColumn>
+				<TableHeaderColumn dataField="site_url" dataFormat={anchor} dataSort={true}>URL</TableHeaderColumn>
+				<TableHeaderColumn dataField="email">Email</TableHeaderColumn>
+				<TableHeaderColumn dataField="username">Username</TableHeaderColumn>
+				<TableHeaderColumn dataField="password">Password</TableHeaderColumn>
+				<TableHeaderColumn dataField="note">Note</TableHeaderColumn>
+				<TableHeaderColumn dataField="tag">Tag</TableHeaderColumn>
+				<TableHeaderColumn dataField="created_at">Created At</TableHeaderColumn>
+				<TableHeaderColumn dataField="updated_at">Updated At</TableHeaderColumn>
 			</BootstrapTable>
 			</div>
 			)
