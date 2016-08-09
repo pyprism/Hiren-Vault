189b8d11a6cf2537929aa135fc0c1ae4919f6e99
Ashutosh Das <git.pyprism@gmail.com>
2016-08-09 23:40:30 +0600

    starting refactoring 3...2...1....lift off :P

--- static/apps/components/Passwords.jsx
+++ static/apps/components/Passwords.jsx
@@ -11,9 +11,36 @@ import {toJS} from 'mobx';
 
 @observer
 export default class Passwords extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bunny : []
+		}
+	}
 
 	componentDidMount = () => {
-		this.props.routes[1].store.fetch();
+		(async function(){
+			let bugs = await axios.get('/api/vault/', {
+				headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+			});
+
+			bugs.forEach(async data => {
+				let temp = {};
+        temp['id'] = data.id;
+        temp['site_url'] = data.site_url;
+        temp['tag'] = data.tag;
+        temp['email'] = await decrypt(sessionStorage.getItem('key'), data.email);
+        temp['username'] = await decrypt(sessionStorage.getItem('key'), data.username);
+        temp['password'] = await decrypt(sessionStorage.getItem('key'), data.password);
+        temp['note'] = await decrypt(sessionStorage.getItem('key'), data.note);
+        temp['created_at'] = data.created_at;
+        temp['updated_at'] = data.updated_at;
+
+		})
+			console.log(this.state.bunny);
+		}.bind(this))();
+		
+		//this.props.routes[1].store.fetch();
 	}
 
 	render() {
@@ -25,14 +52,14 @@ export default class Passwords extends React.Component {
 			else if (cell.startsWith('https://'))
 				a = cell.slice(8);
 			return '<a href=' + cell + '>' + a + '</a>' ;
-  		}
-
-		return (
-			<div>
-			<Helmet
-			title="Vault-> Passwords"
-			/>
-			<BootstrapTable data={toJS(this.props.routes[1].store.bunny)} striped={true} hover={true} condensed={true} pagination={true} search={true}>
+		}
+		if(this.state.bunny == []) {
+			return (
+				<div>
+				<Helmet
+				title="Vault-> Passwords"
+				/>
+				<BootstrapTable data={this.state.bunny} striped={true} hover={true} condensed={true} pagination={true} search={true}>
 				<TableHeaderColumn dataField="id" isKey={true}>ID</TableHeaderColumn>
 				<TableHeaderColumn dataField="site_url" dataFormat={anchor} dataSort={true}>URL</TableHeaderColumn>
 				<TableHeaderColumn dataField="email">Email</TableHeaderColumn>
@@ -42,8 +69,10 @@ export default class Passwords extends React.Component {
 				<TableHeaderColumn dataField="tag">Tag</TableHeaderColumn>
 				<TableHeaderColumn dataField="created_at">Created At</TableHeaderColumn>
 				<TableHeaderColumn dataField="updated_at">Updated At</TableHeaderColumn>
-			</BootstrapTable>
-			</div>
-			)
+				</BootstrapTable>
+				</div>
+				)
+		}
+		return <div>loading.... </div>
 	}
 }
