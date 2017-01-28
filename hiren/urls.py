7ffaf31c6d63cb132b8b84660e3e4543fc715194
Ashutosh Das <git.pyprism@gmail.com>
2017-01-29 00:27:49 +0600

    removed unused codes

--- hiren/urls.py
+++ hiren/urls.py
@@ -21,15 +21,12 @@ from password import views
 
 router = routers.DefaultRouter()
 router.register('vault', views.VaultViewSet)
-router.register('tag', views.TagViewSet)
 router.register('recent', views.RecentViewSet)
-router.register('secret', views.SecretViewset)
 
 urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
