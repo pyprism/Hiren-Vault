cc5ebeaaaa563375efb43aa5bdc7d0502135cd3e
Ashutosh Das <git.pyprism@gmail.com>
2017-01-29 10:23:15 +0600

    added new field to meta class

--- password/serializers.py
+++ password/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Vault, Recent
+from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
 
 
-class VaultSerializer(serializers.ModelSerializer):
+class VaultSerializer(TaggitSerializer, serializers.ModelSerializer):
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
+    tag = TagListSerializerField()
 
     class Meta:
         model = Vault
+        fields = '__all__'
 
 
 class RecentSerializer(serializers.ModelSerializer):
@@ -15,3 +18,8 @@ class RecentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recent
+        fields = '__all__'
+
+
+class TagsListSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
