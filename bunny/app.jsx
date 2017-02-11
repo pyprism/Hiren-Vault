aaad69b532c6f45348b72497bad1dac69fb7cadc
Ashutosh Das <git.pyprism@gmail.com>
2017-02-11 18:36:58 +0600

    added new route

--- bunny/app.jsx
+++ bunny/app.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import axios from 'axios';
 import Login from './components/Login.jsx';
+import Secret from './components/Secret.jsx';
+import Main from './components/Main.jsx';
+import All from './components/All.jsx';
+import Form from './components/Form.jsx';
 
 function authRequired(nextState, replace) {
     let token = sessionStorage.getItem('token');
@@ -28,15 +32,16 @@ function authRequired(nextState, replace) {
 ReactDOM.render(
     <Router history={browserHistory} >
         <Route path="/" component={Login} />
-       {/**  <Route path="/secret" component={Secret} />
-        <Route path="/dashboard" onEnter={authRequired} component={Main}>
-            <IndexRoute posts={ diary } component={Posts}/>
-            <Route path="posts" posts={ diary } component={Posts} />
-            <Route path="posts/:id/" posts={ diary } component={Post} />
-            <Route path="posts/:id/edit" posts={ diary } component={PostEdit} />
+        <Route path="/secret" component={Secret} />
+       <Route path="/dashboard" onEnter={authRequired} component={Main}>
+            <IndexRoute component={All}/>
+            <Route path="all"  component={All} />
             <Route path="new" component={Form} />
-            <Route path="notes" notes={notes} component={Notes} />
-        </Route> **/}
+       {/**       <Route path="posts/:id/" posts={ diary } component={Post} />
+            <Route path="posts/:id/edit" posts={ diary } component={PostEdit} />
+
+            <Route path="notes" notes={notes} component={Notes} /> **/}
+        </Route>
 
     </Router>,
     document.getElementById('app')
