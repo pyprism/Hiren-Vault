77094de91a17c2e399bd6044bcb06364c9916b0e
Ashutosh Das <git.pyprism@gmail.com>
2017-02-12 16:52:58 +0600

    Added new function in form component

--- bunny/components/Form.jsx
+++ bunny/components/Form.jsx
@@ -8,7 +8,63 @@ import Crypt from '../utils/Crypt.jsx';
 export default class Form extends React.Component {
 
     form(e){
-        
+        /***
+         * Handle post save
+         */
+        e.preventDefault();
+        console.log('hit');
+                // key, salt generation
+        let  random = forge.random.getBytesSync(32),
+            _salt = forge.random.getBytesSync(128),
+            key = forge.pkcs5.pbkdf2(sessionStorage.getItem('key'), _salt, 1500, 32);
+
+        var tagStr = ReactDOM.findDOMNode(this.refs.tag).value;
+        var _tag =[];
+        var len = tagStr.length;
+        var hiren;
+        var x = "";
+        for( hiren=0; hiren<len; hiren++) {
+            if(tagStr[hiren] != ";"){
+                x = x + tagStr[hiren];
+                if( hiren + 1 == len)
+                    _tag.push(x);
+            }
+            else{
+                _tag.push(x);
+                x = "";
+            }
+}
+    }
+
+    componentDidMount() {
+
+        (function () { // function for selectize
+            axios.get('/api/tags/', {
+                headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+            }).then(function (response) {
+                let nisha = [];
+                response.data.map(function (hiren) {
+                    let bunny = {'value': '', 'text': ''};
+                    bunny['value'] = hiren.name;
+                    bunny['text'] = hiren.name;
+                    nisha.push(bunny);
+                });
+                $('#tags').selectize({
+                    delimiter: ';',
+                    persist: false,
+                    options: nisha,
+                    create: function(input) {
+                        return {
+                            value: input,
+                            text: input
+                        }
+                    }
+                })
+            }).catch(function (error) {
+                console.error(error);
+            })
+        })();
+
     }
 
     render() {
@@ -17,39 +73,15 @@ export default class Form extends React.Component {
             <div >
                 <div className="row">
                     <Helmet
-                        title="Hiren-Diary: Form"
+                        title="Hiren-Password: Form"
                     />
                 </div>
 
                 <form className="form-horizontal" onSubmit={this.form.bind(this)}>
-                    <div className="form-group">
-                        <label className="control-label col-sm-2">Post Type</label>
-                        <div className="col-sm-10">
-                            <select className="form-control " ref="postType" onChange={this.postType.bind(this)} required>
-                                <option value="diary">Diary</option>
-                                <option value="note">Note</option>
-                            </select>
-                        </div>
-                    </div>
                     <div className="form-group" id="title">
                         <label className="control-label col-sm-2" >Title</label>
                         <div className="col-sm-10">
                             <input type="text" className="form-control" ref="title" placeholder="Post Title"/>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label className="control-label col-sm-2" >Content</label>
-                        <div className="col-sm-10">
-                            <textarea id="summernote" ref="content" className="form-control" placeholder="Enter Content"/>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label className="control-label col-sm-2">Date</label>
-                        <div className='input-group date' id='datetimepicker'>
-                            <input type='text' ref="date" className="form-control" />
-                                    <span className="input-group-addon">
-                                        <span className="glyphicon glyphicon-calendar" />
-                                    </span>
                         </div>
                     </div>
                     <div className="form-group" >
@@ -66,5 +98,5 @@ export default class Form extends React.Component {
                 </form>
             </div>
         )
-}
+    }
 }
