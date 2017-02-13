30dde193363d24b8aa877ed4cf54650c133901a4
Ashutosh Das <git.pyprism@gmail.com>
2017-02-13 18:04:43 +0600

    refactored login model

--- bunny/models/forms/LoginForm.js
+++ bunny/models/forms/LoginForm.js
@@ -1,12 +1,14 @@
 /**
  * Created by prism on 2/12/17.
  */
-import validatorjs from 'validatorjs';
-import MobxReactForm from 'mobx-react-form';
+ import validatorjs from 'validatorjs';
+ import MobxReactForm from 'mobx-react-form';
+ import axios from 'axios';
+ import { browserHistory } from 'react-router';
 
-const plugins = { dvr: validatorjs };
+ const plugins = { dvr: validatorjs };
 
-const fields = {
+ const fields = {
   username: {
     label: 'Username',
     placeholder: 'Insert Username',
@@ -22,16 +24,32 @@ const fields = {
 class LoginForm extends MobxReactForm {
 
   onSuccess(form) {
-    console.log('Form is valid! Send the request here.');
-    // get field values
-    console.log('Form Values!', form.values());
-  }
+    let values = form.values();
+    axios({
+      method: 'post',
+      url: '/api-token-auth/',
+      data: {
+        'username': values.username,
+        'password': values.password
+      }
+    }).then(function (response) {
+      if (response.data['token']) {
+        sessionStorage.setItem('token', response.data['token']);
+        form.clear();
+        browserHistory.push('/secret/');
+      }
+    })
+    .catch(function (response) {
+      console.log(response)
+      sweetAlert("Oops!", 'Username/Password is not correct', "error");
+    });
+  };
 
   onError(form) {
     // get all form errors
     console.log('All form errors', form.errors());
     // invalidate the form with a custom error message
-    form.invalidate('This is a generic error message!');
+    form.invalidate('Form is not valid!');
   }
 }
 
