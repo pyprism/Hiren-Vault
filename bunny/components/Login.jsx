af9c5b86181a11d2b74a93119811b6e337a3bfbb
Ashutosh Das <git.pyprism@gmail.com>
2017-02-13 18:04:07 +0600

    refactored login component

--- bunny/components/Login.jsx
+++ bunny/components/Login.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Helmet from "react-helmet";
-import axios from 'axios';
-import { browserHistory } from 'react-router';
-import LoginForm from '../models/forms/Login';
+import LoginForm from '../models/forms/LoginForm';
+import { observer } from "mobx-react";
 
+@observer
 export default class Login extends React.Component {
 
     /*login(e) {
@@ -25,51 +25,51 @@ export default class Login extends React.Component {
             .catch(function (response) {
                 sweetAlert("Oops!", 'Username/Password is not correct', "error");
             });
-    }*/
+        }*/
 
-    render () {
-        return <div className="wrapper">
+        render () {
+            return <div className="wrapper">
             <Helmet
-                title="Hiren-Password: Login"
-                link={[
-                    {"rel": "stylesheet", "href": "/static/css/bootstrap.min.css"},
-                    {"rel": "stylesheet", "href": "/static/css/login.css"},
-                    {"rel": "icon", "href": "/static/favicon.ico"},
-                    {"rel": "stylesheet", "type": "text/css", "href": "/static/css/sweetalert.css"}
+            title="Hiren-Password: Login"
+            link={[
+                {"rel": "stylesheet", "href": "/static/css/bootstrap.min.css"},
+                {"rel": "stylesheet", "href": "/static/css/login.css"},
+                {"rel": "icon", "href": "/static/favicon.ico"},
+                {"rel": "stylesheet", "type": "text/css", "href": "/static/css/sweetalert.css"}
                 ]}
-            />
+                />
             {/**  <form className="form-signin" onSubmit={this.login.bind(this)} >
              <h2 className="form-signin-heading">Please login</h2>
              <input type="text" className="form-control" ref="username" placeholder="Username" required="" autoFocus />
              <input type="password" className="form-control" ref="password" placeholder="Password" required/>
              <button className="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-             </form> **/}
+         </form> **/}
 
 
-            <form>
-                <label htmlFor={LoginForm.$('username').id}>
-                    {LoginForm.$('username').label}
-                </label>
-                <input {...LoginForm.$('username').bind()} />
-                <p>{LoginForm.$('username').error}</p>
+         <form className="form-signin">
+         <h2 className="form-signin-heading">Please login</h2>
+         <label htmlFor={LoginForm.$('username').id}>
+         {LoginForm.$('username').label}
+         </label>
+         <input className="form-control" {...LoginForm.$('username').bind()} autoFocus  />
+         <p>{LoginForm.$('username').error}</p>
 
-                <label htmlFor={LoginForm.$('password').id}>
-                    {LoginForm.$('password').label}
-                </label>
-                <input {...LoginForm.$('password').bind({ type: 'password'})} />
-                    <p>{LoginForm.$('password').error}</p>
+         <label htmlFor={LoginForm.$('password').id}>
+         {LoginForm.$('password').label}
+         </label>
+         <input className="form-control" {...LoginForm.$('password').bind({ type: 'password'})} />
+         <p>{LoginForm.$('password').error}</p>
 
-                    <button type="submit" onClick={LoginForm.onSubmit}>Submit</button>
-                    <button type="button" onClick={LoginForm.onReset}>Reset</button>
-                    <button type="button" onClick={LoginForm.onClear}>Clear</button>
+         <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={LoginForm.onSubmit}>Login</button>
+                {/**  <button type="button" onClick={LoginForm.onReset}>Reset</button>
+            <button type="button" onClick={LoginForm.onClear}>Clear</button>  **/}
 
-                    <p>{LoginForm.error}</p>
-                    </form>
-
-
-                    </div>
+            <p>{LoginForm.error}</p>
+            </form>
 
 
-                    }
+            </div>
 
-                }
+
+        }
+    }
