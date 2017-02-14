3c106dfbbf1f6e2d90390f2adbb19c41203cb7cb
Ashutosh Das <git.pyprism@gmail.com>
2017-02-14 11:36:12 +0600

    changed navbar position

--- bunny/components/Main.jsx
+++ bunny/components/Main.jsx
@@ -63,7 +63,7 @@ export default class Main extends React.Component {
                 ]}
                 />
 
-                <nav className="navbar navbar-default no-margin navbar-fixed-bottom navbar-inverse">
+                <nav className="navbar navbar-default no-margin navbar-fixed-top navbar-inverse">
                     <div className="navbar-header fixed-brand">
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"  id="menu-toggle">
                             <span className="glyphicon glyphicon-th-large" aria-hidden="true" />
