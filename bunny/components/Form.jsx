af1e00ae5f9206f760cb01265779897c49ffb9b2
Ashutosh Das <git.pyprism@gmail.com>
2017-02-14 11:36:37 +0600

    wip

--- bunny/components/Form.jsx
+++ bunny/components/Form.jsx
@@ -55,7 +55,7 @@ export default class Form extends React.Component {
                     bunny['text'] = hiren.name;
                     nisha.push(bunny);
                 });
-                $('#tags').selectize({
+                $('.tag').selectize({
                     delimiter: ';',
                     persist: false,
                     options: nisha,
@@ -117,7 +117,7 @@ export default class Form extends React.Component {
                     <div className="form-group" >
                         <label className="control-label col-sm-2" htmlFor={NewPasswordForm.$('note').id} > {NewPasswordForm.$('note').label} </label>
                         <div className="col-sm-10">
-                            <input className="form-control" {...NewPasswordForm.$('note').bind()} />
+                            <textarea className="form-control" {...NewPasswordForm.$('note').bind()} rows="3" />
                             <span className='error text-danger' >{NewPasswordForm.$('note').error}</span>
                         </div>
                     </div>
@@ -138,25 +138,28 @@ export default class Form extends React.Component {
                     <div className="form-group" >
                         <label className="control-label col-sm-2" htmlFor={NewPasswordForm.$('tag').id} > {NewPasswordForm.$('tag').label} </label>
                         <div className="col-sm-10">
-                            <input className="form-control" {...NewPasswordForm.$('tag').bind()} />
+                            <input className="form-control tag" {...NewPasswordForm.$('tag').bind()} />
                             <span className='error text-danger' >{NewPasswordForm.$('tag').error}</span>
                         </div>
                     </div>
 
-                    <div className="form-group" >
-                        <label className="control-label col-sm-2" htmlFor={NewPasswordForm.$('audit').id} > {NewPasswordForm.$('audit').label} </label>
-                        <div className="col-sm-10">
-                            <input className="form-control" {...NewPasswordForm.$('audit').bind()} />
-                            <span className='error text-danger' >{NewPasswordForm.$('audit').error}</span>
+                    <div className="form-group">
+                        <div className="col-sm-offset-2 col-sm-10">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" {...NewPasswordForm.$('audit').bind()} /> Enable Audit
+                                    <span className='error text-danger' >{NewPasswordForm.$('audit').error}</span>
+                                </label>
+                            </div>
                         </div>
                     </div>
                     <div className="form-group">
                         <div className="col-sm-offset-2 col-sm-10">
-                            <button type="submit" className="btn btn-default"><i className="fa fa-save" /> Save</button>
+                            <button type="submit" className="btn btn-default" onClick={NewPasswordForm.onSubmit}><i className="fa fa-save" /> Save</button>
                         </div>
                     </div>
                 </form>
             </div>
-        )
+    )
     }
-}
+    }
