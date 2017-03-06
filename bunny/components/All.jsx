2df8aacf325c23ea084aa2cf8b504a28c9c9b948
Ashutosh Das <git.pyprism@gmail.com>
2017-03-06 19:18:21 +0600

    added table

--- bunny/components/All.jsx
+++ bunny/components/All.jsx
@@ -10,16 +10,59 @@ import {Link} from 'react-router';
 @observer
 export default class All extends React.Component {
 
-	componentDidMount(){
+    componentDidMount(){
         this.props.route.passwords.getData();
-        console.log('component');
+
+        // table conf
+        $('#table').bootstrapTable({
+            pagination: true,
+            pageSize: 22,
+            search: true,
+            sortable: true,
+            columns: [{
+                field: 'id',
+                title: 'ID'
+            }, {
+                field: 'site_url',
+                title: 'URL'
+            }, {
+                field: 'email',
+                title: 'Email'
+            }, {
+                field: 'username',
+                title: 'Username'
+            }, {
+                field: 'created_at',
+                title: 'Created At'
+            }],
+            data: toJS(this.props.route.passwords.passwords),
+            onClickCell: function (field, value, row, $element) {
+                if(field == 'site_url'){
+                    //return '<a target="_blank" href=' + value + '>'+ value + '</a>' ;
+                     //var win = window.open(value, '_blank');
+                    //window.open(value, '_blank');
+                    //win.focus();
+                    browserHistory.push('/dashboard/' + row['id'] + '/');
+                }
+            }
+        });
     }
 
     render() {
-        return (
-            <div>
-                i am ur "all"
-            </div>
-        )
+        if(this.props.route.passwords.loaded){
+            return(
+                <div>
+                    <Helmet
+                        title="Hiren-Vault: All Passwords"
+                    />
+                    <table id="table"></table>
+                </div>
+            )} else {
+            return (
+                <div>
+                    <h3>{this.props.route.passwords.loadingText} </h3>
+                </div>
+            )
+        }
     }
 }
