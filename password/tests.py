b01ca6ac25295a09ab81b215e4227851fc4d7b1f
Ashutosh Das <git.pyprism@gmail.com>
2017-06-29 10:14:33 +0600

    fixed model test

--- password/tests.py
+++ password/tests.py
@@ -3,45 +3,38 @@ from .models import Recent, Vault
 from django.utils import timezone
 from rest_framework.test import APIRequestFactory, APIClient
 from django.contrib.auth.models import User
-from mock import patch
+# from mock import patch
 from contextlib import contextmanager
 import datetime
 from django.utils import timezone
 
 
-# class ModelTest(TransactionTestCase):
-#     """
-#     Test all models
-#     """
-#     reset_sequences = True
+class ModelTest(TransactionTestCase):
+    """
+    Test all models
+    """
+    reset_sequences = True
 
-#     def setUp(self):
-#         tag = Tag.objects.create(name='tag object')
-#         vault = Vault.objects.create(site_url='http://xyz.com', username='prism',
-#                                      email='a@x.com', password='1234', note='note', tag=tag)
-#         Recent.objects.create(vault=vault)
+    def setUp(self):
+        vault = Vault.objects.create(site_url='http://xyz.com', username='prism',
+                                     email='a@x.com', password='1234', note='note',
+                                     title='title', iv='sdfc', salt='sasasa', iteration=2)
+        Recent.objects.create(vault=vault)
 
-#     def test_tag_model(self):
-#         tag_item = Tag.objects.all()
-#         self.assertEqual(tag_item.count(), 1)
+    def test_vault_model(self):
+        vault_items = Vault.objects.all()
+        self.assertEqual(vault_items.count(), 1)
+        self.assertEqual(vault_items[0].site_url, 'http://xyz.com')
+        self.assertEqual(vault_items[0].username, 'prism')
+        self.assertEqual(vault_items[0].email, 'a@x.com')
+        self.assertEqual(vault_items[0].password, '1234')
+        self.assertEqual(vault_items[0].note, 'note')
+        # self.assertEqual(vault_items[0].tag.id, 1)
 
-#         tag_result = Tag.objects.get(name="tag object")
-#         self.assertEqual(tag_result.name, "tag object")
-
-#     def test_vault_model(self):
-#         vault_items = Vault.objects.all()
-#         self.assertEqual(vault_items.count(), 1)
-#         self.assertEqual(vault_items[0].site_url, 'http://xyz.com')
-#         self.assertEqual(vault_items[0].username, 'prism')
-#         self.assertEqual(vault_items[0].email, 'a@x.com')
-#         self.assertEqual(vault_items[0].password, '1234')
-#         self.assertEqual(vault_items[0].note, 'note')
-#         self.assertEqual(vault_items[0].tag.id, 1)
-
-#     def test_recent_model(self):
-#         recent_items = Recent.objects.all()
-#         self.assertEqual(recent_items.count(), 1)
-#         self.assertEqual(recent_items[0].vault.id, 1)
+    def test_recent_model(self):
+        recent_items = Recent.objects.all()
+        self.assertEqual(recent_items.count(), 1)
+        self.assertEqual(recent_items[0].vault.id, 1)
 
 
 # class VaultViewSetTest(TransactionTestCase):
