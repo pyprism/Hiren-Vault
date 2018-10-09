f4b956247909a315746cc637c744640d94ba32e5
Ashutosh Das <git.pyprism@gmail.com>
2018-10-09 20:56:07 +0600

    nuked

--- bunny/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
-import Login from "component/Login";
-
-
-ReactDOM.render(<Login />, document.getElementById('root'));
-registerServiceWorker();
