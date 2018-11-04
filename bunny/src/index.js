99b5fd9970359afe6c78de9c30ac328e7ee80d1f
Ashutosh Das <git.pyprism@gmail.com>
2018-11-04 21:18:05 +0600

    added new routes

--- bunny/src/index.js
+++ bunny/src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import App from './App';
+import Login from "components/pages/Login";
+import Register from "components/pages/Register";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route path="/" exact component={Login} />
+                    <Route path="/register" exact component={Register} />
+                    <Route path="/music" component={App} />
+                </div>
+            </Router>
+        )
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
